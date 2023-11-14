e90dd989fa1beff7c20825582f9e9d30f0614220
orbyfied <atomf88zero@gmail.com>
2023-11-14 17:11:49 +0100

    added IOUtil#download

--- veru-java8/src/main/java/slatepowered/veru/io/IOUtil.java
+++ veru-java8/src/main/java/slatepowered/veru/io/IOUtil.java
@@ -1,8 +1,11 @@
 package slatepowered.veru.io;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import slatepowered.veru.misc.Throwables;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class IOUtil {
 
@@ -49,6 +52,40 @@ public class IOUtil {
      */
     public static void transfer(InputStream inputStream, OutputStream outputStream) throws IOException {
         transfer(inputStream, outputStream, 1024);
+    }
+
+    /**
+     * 'Safely' parses a URL, basically just constructs
+     * the URL silently rethrowing any errors.
+     *
+     * @param url The URL string.
+     * @return The URL object.
+     */
+    public static URL parseURL(String url) {
+        try {
+            return new URL(url);
+        } catch (Throwable t) {
+            Throwables.sneakyThrow(t);
+            throw new AssertionError();
+        }
+    }
+
+    /**
+     * Download the given URL to the given file path.
+     *
+     * @param url The URL.
+     * @param output The output file.
+     */
+    public static void download(URL url, Path output) {
+        try {
+            BufferedInputStream inputStream = new BufferedInputStream(url.openStream());
+            FileUtil.createDirectoryIfAbsent(output.getParent());
+            BufferedOutputStream outputStream = new BufferedOutputStream(Files.newOutputStream(output));
+            outputStream.close();
+            inputStream.close();
+        } catch (Throwable t) {
+            throw new RuntimeException("Error while downloading file " + url, t);
+        }
     }
 
 }
