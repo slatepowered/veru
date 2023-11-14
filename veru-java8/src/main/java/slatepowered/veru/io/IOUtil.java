3ef3cc9e317a5ab67e7fad7909dff20d890331eb
orbyfied <atomf88zero@gmail.com>
2023-11-14 17:08:17 +0100

    add IOUtil#transfer

--- veru-java8/src/main/java/slatepowered/veru/io/IOUtil.java
+++ veru-java8/src/main/java/slatepowered/veru/io/IOUtil.java
@@ -2,6 +2,7 @@ package slatepowered.veru.io;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 public class IOUtil {
 
@@ -15,6 +16,39 @@ public class IOUtil {
         byte[] arr = new byte[stream.available()];
         stream.read(arr);
         return arr;
+    }
+
+    /**
+     * Transfers the given input stream to the output stream
+     * using buffering.
+     *
+     * @param inputStream The input stream.
+     * @param outputStream The output stream.
+     * @param bufferSize The buffer size.
+     * @throws IOException Any IO exception.
+     */
+    public static void transfer(InputStream inputStream, OutputStream outputStream, int bufferSize) throws IOException {
+        byte[] buffer = new byte[bufferSize];
+        int len = inputStream.read(buffer);
+        while (len != -1) {
+            outputStream.write(buffer, 0, len);
+            len = inputStream.read(buffer);
+        }
+    }
+
+    /**
+     * Transfers the given input stream to the output stream
+     * using buffering, using {@link #transfer(InputStream, OutputStream, int)}
+     * internally.
+     *
+     * It uses a buffer size of 1024.
+     *
+     * @param inputStream The input stream.
+     * @param outputStream The output stream.
+     * @throws IOException Any IO exception.
+     */
+    public static void transfer(InputStream inputStream, OutputStream outputStream) throws IOException {
+        transfer(inputStream, outputStream, 1024);
     }
 
 }
