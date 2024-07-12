5a975a05a192184b6b96a2489354c9fe80120d8f
orbyfied <atomf88zero@gmail.com>
2024-07-12 17:46:36 +0200

    gg

--- veru-java8/src/main/java/slatepowered/veru/misc/ANSI.java
+++ veru-java8/src/main/java/slatepowered/veru/misc/ANSI.java
@@ -9,14 +9,24 @@ public class ANSI {
 
     /* Formatting Codes */
     public static final String RESET = "\u001B[0m";
+
     public static final String BLACK = "\u001B[30m";
-    public static final String RED = "\u001B[31m";
-    public static final String GREEN = "\u001B[32m";
-    public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
-    public static final String PURPLE = "\u001B[35m";
-    public static final String CYAN = "\u001B[36m";
-    public static final String WHITE = "\u001B[37m";
+    public static final String DARK_RED = "\u001B[31m";
+    public static final String DARK_GREEN = "\u001B[32m";
+    public static final String DARK_YELLOW = "\u001B[33m";
+    public static final String DARK_BLUE = "\u001B[34m";
+    public static final String DARK_PURPLE = "\u001B[35m";
+    public static final String DARK_CYAN = "\u001B[36m";
+    public static final String BRIGHT_GRAY = "\u001B[37m";
+
+    public static final String GRAY = "\u001B[90m";
+    public static final String RED = "\u001B[91m";
+    public static final String GREEN = "\u001B[92m";
+    public static final String YELLOW = "\u001B[93m";
+    public static final String BLUE = "\u001B[94m";
+    public static final String PURPLE = "\u001B[95m";
+    public static final String CYAN = "\u001B[96m";
+    public static final String WHITE = "\u001B[97m";
 
     //
 
