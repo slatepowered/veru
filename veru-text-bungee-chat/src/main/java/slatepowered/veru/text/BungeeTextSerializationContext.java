13044188bc45075cb4c159ad36128ed3f6e56623
orbyfied <atomf88zero@gmail.com>
2026-03-27 17:19:24 +0100

    del

--- veru-text-bungee-chat/src/main/java/slatepowered/veru/text/BungeeTextSerializationContext.java
+++
@@ -1,7 +0,0 @@
-package slatepowered.veru.text;
-
-import net.md_5.bungee.api.chat.ComponentBuilder;
-
-public class BungeeTextSerializationContext extends TextSerializationContext<ComponentBuilder> {
-
-}
